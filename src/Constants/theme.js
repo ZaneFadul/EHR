a76e4d89fcebf77a9a8206d18d0111c1a10eea3c
ZaneFadul <theboonandthezane@gmail.com>
2020-11-17 00:34:45 -0500

    more theme files, just gonna push

---
+++ src/Constants/theme.js
@@ -0,0 +1,30 @@
+const theme = {
+  "mainColors": {
+    "black": "000000",
+    "white": "",
+    "": ""
+  },
+  "roleColors":
+  {
+    "patient":
+    {
+      "primary": "",
+      "secondary": "",
+      "accent": ""
+    },
+    "staff":
+    {
+      "primary": "",
+      "secondary": "",
+      "accent": ""
+    },
+    "insurance":
+    {
+      "primary": "",
+      "secondary": "",
+      "accent": ""
+    }
+  }
+};
+
+export default theme;
---
+++ src/Constants/theme.test.js
@@ -0,0 +1,6 @@
+import theme from './theme';
+
+//Example Theme
+test("Black really is black", () => {
+  expect(theme.mainColors.black).toBe("000000")
+});
