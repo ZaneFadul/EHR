3f528f9f18014c9aa88df420f8ad9e4348759e3c
NotLiu <aliu@lediur.com>
2020-11-23 01:42:51 -0600

    Defined Patient Schema

--- app/models/organizations.js
+++ app/models/organizations.js
@@ -1,5 +1,7 @@
 // Define JS class and MongoDB components for Organizations
 
+const mongoose = require("mongoose");
+
 // Define organization JS class
 class Organization{
     constructor(orgid, name, type, info){
@@ -15,7 +17,21 @@ class Organization{
 
 
 // Define organization MongoDB component
-
+const schema_organization = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "organization name is required"]
+    },
+    type: {
+        type: String,
+        required: [true, "organization type is required"]
+    }
+    info: {
+        type: String,
+        required: [true, "organization info is required"]
+    }
+});
 
 // export module
 module.exports.orgs = Organization;
+module.exports.schema_organization = schema_organization;
