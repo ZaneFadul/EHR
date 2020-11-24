a52d6eb7ff2338ce9f669450a2a6871bb97cba03
NotLiu <aliu@lediur.com>
2020-11-24 02:35:11 -0600

    Defined prescription MongoDB schema

--- app/models/prescription.js
+++ app/models/prescription.js
@@ -1,6 +1,6 @@
-// Define JS class and MongoDB components for Patients
+// Define JS class and MongoDB components for Prescriptions
 const mongoose = require("mongoose");
-// Define Patients JS class
+// Define Prescriptions JS class
 
 class Prescription{
     constructor(medications, date, description, start_date, end_date){
@@ -15,8 +15,29 @@ class Prescription{
     }
 };
 
-// Define Patients MongoDB component
-
+// Define Prescription MongoDB component
+const schema_prescription = new mongoose.Schema({
+    medications: {
+        type: class,
+        required: [true, "medications required"]
+    },
+    date: {
+        type: Date,
+        required: [true, "type is required"]
+    },
+    description: {
+        type: String,
+        required: [true, "description is required"]
+    },
+    start_date: {
+        type: Date,
+        required: [true, "start date is required"]
+    }
+    end_date: {
+        type: Date,
+        required: [true, "end date is required"]
+    }
+})
 // export module
 module.exports.prescription = Prescription;
 module.exports.schema_prescription = schema_prescription;
