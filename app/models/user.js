30826f87924606244c120d10a3b282197a4fc84c
NotLiu <aliu@lediur.com>
2020-11-23 20:36:00 -0600

    Created branch, created user.js template

---
+++ app/models/user.js
@@ -0,0 +1,14 @@
+//Define JS class and MongoDB schema for Users
+const mongoose = require("mongoose");
+//Define JS class
+class user{
+
+};
+
+//Define MongoDB schema
+const schema_user = new mongoose.Schema({
+
+});
+
+module.exports.user = user;
+module.exports.schema_user = schema_user;
