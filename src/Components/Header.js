95302ee657b03785269ed74a1779c52b9acfbaef
FreyaXiang <xiangxin0511@gmail.com>
2020-11-27 19:35:49 +0800

    add logout and border

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,23 +1,33 @@
-import React, { Component } from 'react';
-import theme from '../Constants/theme';
-import './Header.css';
-import Menu from './Menu';
+import React from "react";
+import theme from "../Constants/theme";
+import Logo from "../Constants/logo_red.svg";
+import "./Header.css";
 
-export default class Header extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      logoOn: false
-    };
-  }
-
-  render() {
-    return (
-      <div className="Header" style={{
-        borderColor : `${theme.roleColors[this.props.role]['primary']}`}}>
-          <Menu />
+export default function Header(props) {
+  return (
+    <div>
+      <div className="Header">
+        <img
+          src={Logo}
+          alt="none"
+          className="Header-logo"
+          // style={{
+          //   animationName: `logo-hover-${this.state.logoPop}`,
+          //   animationDuration: "0.3s",
+          //   animationTimingFunction: "ease-out",
+          //   animationFillMode: "both",
+          // }}
+        />
+        <a href="/login" className="logout">
+          <i class="material-icons right">exit_to_app</i>
+          <span>Logout</span>
+        </a>
       </div>
-    );
-  }
+      <hr
+        style={{
+          borderColor: `${theme.roleColors[props.role]["primary"]}`,
+        }}
+      ></hr>
+    </div>
+  );
 }
-
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import Logo from '../Constants/logo_red.svg';
-import './Menu.css';
+import React, { Component } from "react";
+import Logo from "../Constants/logo_red.svg";
+import "./Menu.css";
 
 export default class HeaderMenu extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      logoPop: false
+      logoPop: false,
     };
     this.toggleLogoPop = this.toggleLogoPop.bind(this);
   }
@@ -14,33 +14,34 @@ export default class HeaderMenu extends Component {
   toggleLogoPop(prevProps, prevState) {
     if (this.state.logoPop === true) {
       this.setState({
-        logoPop: false
+        logoPop: false,
       });
     } else {
       this.setState({
-        logoPop: true
+        logoPop: true,
       });
     }
   }
 
   render() {
-    
     return (
       <div className="Header-menu">
-        <button className="Header-menu-item">Dashboard</button>
-        <button className="Header-menu-item">About</button>
-      <img src={Logo} alt="none"
-        className="Header-logo"
-        onMouseOver={this.toggleLogoPop}
-        onMouseOut={this.toggleLogoPop}
-        style={{
-          animationName: `logo-hover-${this.state.logoPop}`,
-          animationDuration: '0.3s',
-          animationTimingFunction: 'ease-out',
-          animationFillMode: 'both',
-        }}
+        {/* <button className="Header-menu-item">Dashboard</button>
+        <button className="Header-menu-item">About</button> */}
+        <img
+          src={Logo}
+          alt="none"
+          className="Header-logo"
+          onMouseOver={this.toggleLogoPop}
+          onMouseOut={this.toggleLogoPop}
+          style={{
+            animationName: `logo-hover-${this.state.logoPop}`,
+            animationDuration: "0.3s",
+            animationTimingFunction: "ease-out",
+            animationFillMode: "both",
+          }}
         />
       </div>
-    )
+    );
   }
 }
