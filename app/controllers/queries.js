39bdff70a45fcc0ae9d46207a1fa8492cca045c4
NotLiu <aliu@lediur.com>
2020-11-30 03:43:15 -0600

    login query implemented and tested

--- app/controllers/queries.js
+++ app/controllers/queries.js
@@ -23,20 +23,45 @@ async function createRecord(model, create_func, p_array){
     console.log(`NEW DOCUMENT : ${result}`);
 }
 
-//account queries
 const ehr_user = mongoose.model('User', user.schema_user);
-
-
 //implement Login Query Function
 function login(username, password){
-    ehr_user.findOne({username:username},function(err,user){
+    user_id = -1;
+    ehr_user.findOne({
+        username:username
+    }, function(err,user_q){
+        user_id = user_q.userID;
         //return -1 if error
-        if()
-    });
-    
+        if(err){
+            console.error(err);
+            return -1;
+        }else{
+        //query password if valid username
+            ehr_user.findOne({
+                password:password
+            }, function(err, pass_q){
+                //return -1 if error
+                if(err){
+                    console.error(err);
+                    return -1;
+                }
+                else{
+                    console.log("USERNAME CHECK");
+                    if(pass_q == null || user_q == null){
+                        return -1;
+                    }
 
-    //return userID
+                    if(pass_q.userID == user_id){
+                        console.log(`${user_id} LOGGED IN`)
+                        return user_id;
+                    }
+                }
+            })
+        }
+    });
 }
+
+
 
 //implement Registration Query Function
 
@@ -45,6 +70,6 @@ function login(username, password){
 
 
 module.exports.login = login;
-module.exports.register = register;
+// module.exports.register = register;
 
 module.exports.createRecord = createRecord;
