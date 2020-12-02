a1dc654ea78f0de766f8e80a51e7e1252b2b3281
Andrew Lee <avllee21@Andrews-MacBook-Pro-5.local>
2020-12-02 18:02:00 +0800

    added hospital staff schema

---
+++ app/models/staff.js
@@ -0,0 +1,33 @@
+// Define JS class and MongoDB components for Patients
+
+const mongoose = require("mongoose");
+
+
+// Define Patients MongoDB component
+
+const schema_staff = new mongoose.Schema({
+    name: {   
+        type: String,
+        required: [true, 'name is required']
+    },
+    position: {   
+        type: String,
+        required: [true, 'position is required']
+    },
+    permissions: {   
+        type: Array,
+        required: [true, 'permissions is required']
+    },
+    patients: {   
+        type: Array,
+        required: [true, 'patients is required']
+    },
+    appointments: {   
+        type: Array,
+        required: [true, 'appointments is required']
+    },
+});
+
+
+// export module
+module.exports.schema_patient = schema_staff;
